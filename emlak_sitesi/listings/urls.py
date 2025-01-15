13621b1dcbb6dcfe84871c6cb53f9e10ca8b76a9
mehmet eski <mehmeteski_40@hotmail.com>
2025-01-15 16:10:05 +0300

    Revert "yaptık bişiler"
    This reverts commit 70b19e7206bc7073e2eabc474aec396b72810d6c.

--- emlak_sitesi/listings/urls.py
+++ emlak_sitesi/listings/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import ListingList
 from .views import ListingDetail
 from .views import ListingCreate, ListingUpdate
-from . import views
 
 urlpatterns = [
     path('listings/', ListingList.as_view(), name='listing-list'),
@@ -12,5 +11,4 @@ urlpatterns = [
     path('listings/<int:pk>/', ListingDetail.as_view(), name='listing-detail'),
     path('listings/create/', ListingCreate.as_view(), name='listing-create'),
     path('listings/update/<int:pk>/', ListingUpdate.as_view(), name='listing-update'),
-    path('search/', views.search_view, name='search'),
 ]
